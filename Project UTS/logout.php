6583148e060d9f754dcaa48c92b579efce2b7a7f
kotongzz <kotongzz@users.noreply.github.com>
2020-11-05 22:56:57 +0800

    Delete Project bentar

--- Project UTS/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-//menghancurkan $_SESSION['user']
-session_destroy();
-echo "<script>alert('Anda telah logout');</script>";
-echo "<script>location='index.php';</script>";
-?>
--- Project UTS/tes.md
+++
@@ -1,4 +0,0 @@
-Kelompok : 
-I Made Adi Susilayasa 1808561089
-i ketut Prawira Adhisastra 1808561078
-I Putu Indie Surya Jayadi 1808561073
